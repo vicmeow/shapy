477ca50cdb79ecb9aac9a1f8cffdacab8c009275
vicbergquist <victoria.bergquist@gmail.com>
2018-10-30 21:29:57 +0100

    slider styles

--- src/components/GradientForm.vue
+++ src/components/GradientForm.vue
@@ -86,9 +86,20 @@ export default {
     flex-direction: column
     text-align: center
     width: 50%
+    flex-basis: 50%
 
-    label
-      font-size: .95rem
+  .number-wrapper
+    display: flex
+    align-items: center
+    justify-content: center
+
+    .label-number
+      display: flex
+      padding: 0
+      min-width: 0
+      max-width: 3.5rem
+      text-align: right
+      padding: 0 .3rem
 
   input
     font-family: $font
@@ -96,7 +107,8 @@ export default {
     color: $black
     border: 0
     padding: .5rem 1rem
-    max-width: 100%
+    &:focus
+      outline: 0
 
   input[type="text"]
     width: 100%
@@ -128,13 +140,106 @@ export default {
     bottom: 0
     display: inline-block
     position relative
-    height: 16px
-    width: 16px
-    border: 1.5px solid $green
+    height: 17px
+    width: 17px
+    border: 1.4px solid $black
     border-radius: 50%
     margin-right: .5rem
 
   input[type="radio"]:checked + label::before
-    background: radial-gradient(85% 85% at 50% 50%, $green 40%, $white 41%) no-repeat 50% 50% / 100% 100%
+    background: radial-gradient(85% 85% at 50% 50%, $green 38%, $white 39%) no-repeat 50% 50% / 100% 100%
+
+  ::-moz-focus-inner, ::-moz-focus-outer
+    border: 0
+    background: $green
+
+  input[type=range]
+    -webkit-appearance: none
+    width: 100%
+    margin: 7px 0
+
+  input[type=range]:focus
+    outline: none
+
+  input[type=range]::-webkit-slider-runnable-track
+    width: 100%
+    height: 2px
+    cursor: pointer
+    box-shadow: 0
+    background: $black
+    &:focus
+      background: $green
+
+  input[type=range]::-webkit-slider-thumb
+    box-shadow: 0
+    border: 1.4px solid $black
+    height: 17px
+    width: 17px
+    border-radius: 100%
+    background: radial-gradient(85% 85% at 50% 50%, $black 38%, $white 39%) no-repeat 50% 50% / 100% 100%
+    cursor: pointer
+    -webkit-appearance: none
+    margin-top: -7px
+
+  input[type=range]:focus::-webkit-slider-runnable-track
+    background: $green
+
+  input[type=range]::-moz-range-track
+    width: 100%
+    height: 2px
+    cursor: pointer
+    box-shadow: 0
+    background: $black
+    border: 0
+
+  input[type=range]::-moz-range-track:focus
+    background: $green
+
+  ::-moz-range-progress
+    height: 2px
+    background: $green
+
+  input[type=range]::-moz-range-thumb
+    box-shadow: 0
+    border: 1.4px solid $black
+    height: 17px
+    width: 17px
+    border-radius: 100%
+    background: radial-gradient(85% 85% at 50% 50%, $green 38%, $white 39%) no-repeat 50% 50% / 100% 100%
+    cursor: pointer
+
+  input[type=range]::-ms-track
+    width: 100%
+    height: 2px
+    cursor: pointer
+    background: transparent
+    border-color: transparent
+    color: transparent
+    &:focus
+      background: $green
+
+  input[type=range]::-ms-fill-lower
+    background: $black
+    border: 0
+    box-shadow: 0
+    &:focus
+      background: $green
+
+  input[type=range]::-ms-thumb
+    box-shadow: 0
+    border: 0
+    height: 17px
+    width: 17px
+    border-radius: 100%
+    background: $black
+    cursor: pointer
+    height: 2px
+
+  input[type=range]:focus::-ms-fill-lower
+    background: $green
+
+  input[type=range]:focus::-ms-fill-upper
+    background: $green
+
 
 </style>
