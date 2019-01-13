029125e2181d378eabcadcef8a5215c386efc81c
vicbergquist <victoria.bergquist@gmail.com>
2019-01-13 16:44:04 +0100

    fix: box min max values

--- src/components/fieldsets/FieldsetBox.vue
+++ src/components/fieldsets/FieldsetBox.vue
@@ -12,6 +12,7 @@
         :label="'Width'"
         :name="'box-size-x'"
         :what="box.size.x"
+        :max="box.size.x.unit === '%' ? 100 : boxMax.x"
       />
 
       <!-- HEIGHT INPUT (%/px) -->
@@ -20,6 +21,7 @@
         :label="'Height'"
         :name="'box-size-y'"
         :what="box.size.y"
+        :max="box.size.y.unit === '%' ? 100 : boxMax.y"
       />
 
       <!-- X AXIS (%) -->
@@ -28,8 +30,8 @@
         :label="'X-axis'"
         :name="'box-x'"
         :what="box.coord.x"
-        :min="-100"
-        :max="200"
+        :min="box.coord.x.unit === '%' ? 0 : axis.min.x"
+        :max="box.coord.x.unit === '%' ? 100 : boxMax.x"
       />
 
       <!-- Y AXIS (%) -->
@@ -38,8 +40,8 @@
         :label="'Y-axis'"
         :name="'box-y'"
         :what="box.coord.y"
-        :min="-100"
-        :max="200"
+        :min="box.coord.y.unit === '%' ? 0 : axis.min.y"
+        :max="box.coord.y.unit === '%' ? 100 : boxMax.y"
       />
     </template>
   </div>
@@ -60,12 +62,45 @@ export default {
     GroupToggle
   },
   data: () => ({
-    hidden: true,
+    hidden: false,
     title: 'Box Details 📦',
     desc: 'The box your shape goes into.'
   }),
   computed: {
-    ...mapFields(['box'])
+    ...mapFields(['box']),
+    boxMax() {
+      // Max box size based on canvas (PX)
+      const CANVAS = this.$store.state.canvas.canvas
+      const X =
+        CANVAS.x.unit === '%'
+          ? Math.round((CANVAS.x.size * CANVAS.x.max) / 100)
+          : CANVAS.x.size
+      const Y =
+        CANVAS.y.unit === '%'
+          ? Math.round((CANVAS.y.size * CANVAS.y.max) / 100)
+          : CANVAS.y.size
+      return { x: X, y: Y }
+    },
+    boxPx() {
+      // Box size in PX
+      const X =
+        this.box.size.x.unit === '%'
+          ? Math.round((this.box.size.x.size / 100) * this.boxMax.x)
+          : this.boxMax.x
+      const Y =
+        this.box.size.y.unit === '%'
+          ? Math.round((this.box.size.y.size / 100) * this.boxMax.y)
+          : this.boxMax.y
+      return { x: X, y: Y }
+    },
+    axis() {
+      // Axis min values
+      const MIN_X =
+        this.box.size.x.unit === '%' ? -this.boxPx.x : -this.box.size.x.size
+      const MIN_Y =
+        this.box.size.y.unit === '%' ? -this.boxPx.y : -this.box.size.y.size
+      return { min: { x: MIN_X, y: MIN_Y } }
+    }
   }
 }
 </script>
