c2172ef363ace022aed9a52828bd245748fc36f8
vicbergquist <victoria.bergquist@gmail.com>
2019-01-13 17:30:47 +0100

    fix: shape min max values

--- src/components/fieldsets/FieldsetShape.vue
+++ src/components/fieldsets/FieldsetShape.vue
@@ -3,7 +3,7 @@
     <!-- TITLE & DESCRIPTION -->
     <legend class="legend" @click="hidden = !hidden">{{ title }}</legend>
     <group-toggle :hidden="hidden" @click.native="hidden = !hidden" />
-    <template v-show="!hidden">
+    <template v-if="!hidden">
       <p class="desc">{{ desc }}</p>
 
       <!-- WIDTH INPUT (%/px) -->
@@ -12,6 +12,7 @@
         :label="'Width'"
         :name="'shape-size-x'"
         :what="shape.size.x"
+        :max="shape.size.x.unit === '%' ? 100 : boxPx.x"
       />
 
       <!-- HEIGHT INPUT (%/px) -->
@@ -20,6 +21,7 @@
         :label="'Height'"
         :name="'shape-size-y'"
         :what="shape.size.y"
+        :max="shape.size.y.unit === '%' ? 100 : boxPx.y"
       />
 
       <!-- X AXIS (%) -->
@@ -28,8 +30,8 @@
         :label="'X-axis'"
         :name="'shape-x'"
         :what="shape.coord.x"
-        :min="-100"
-        :max="200"
+        :min="shape.coord.x.unit === '%' ? 0 : axis.min.x"
+        :max="shape.coord.x.unit === '%' ? 100 : boxPx.x + shapePx.x"
       />
 
       <!-- Y AXIS (%) -->
@@ -38,8 +40,8 @@
         :label="'Y-axis'"
         :name="'shape-y'"
         :what="shape.coord.y"
-        :min="-100"
-        :max="200"
+        :min="shape.coord.y.unit === '%' ? 0 : axis.min.y"
+        :max="shape.coord.y.unit === '%' ? 100 : boxPx.y + shapePx.y"
       />
     </template>
   </div>
@@ -60,13 +62,62 @@ export default {
     GroupToggle
   },
   data: () => ({
-    hidden: true,
+    hidden: false,
     title: 'Shape in Box 📐',
     desc:
       'Edit the size and placement of the shape inside the box you edit above.'
   }),
   computed: {
-    ...mapFields(['shape'])
+    ...mapFields(['shape']),
+    boxMax() {
+      // Max box size based on canvas (PX)
+      const CANVAS = this.$store.state.canvas.canvas
+      const X =
+        CANVAS.x.unit === '%'
+          ? Math.round((CANVAS.x.size * CANVAS.x.max) / 100)
+          : CANVAS.x.size
+      const Y =
+        CANVAS.y.unit === '%'
+          ? Math.round((CANVAS.y.size * CANVAS.y.max) / 100)
+          : CANVAS.y.size
+      return { x: X, y: Y }
+    },
+    boxPx() {
+      // Box size in PX
+      const BOX = this.$store.state.box.box
+      const X =
+        BOX.size.x.unit === '%'
+          ? Math.round((BOX.size.x.size / 100) * this.boxMax.x)
+          : this.boxMax.x
+      const Y =
+        BOX.size.y.unit === '%'
+          ? Math.round((BOX.size.y.size / 100) * this.boxMax.y)
+          : this.boxMax.y
+      return { x: X, y: Y }
+    },
+    shapePx() {
+      const X =
+        this.shape.size.x.unit === '%'
+          ? Math.round((this.boxPx.x / 100) * this.shape.size.x.size)
+          : this.shape.size.x.size
+      const Y =
+        this.shape.size.y.unit === '%'
+          ? Math.round((this.boxPx.y / 100) * this.shape.size.y.size)
+          : this.shape.size.y.size
+      return { x: X, y: Y }
+    },
+    axis() {
+      // Axis min values
+      const MIN_X =
+        this.shape.size.x.unit === '%'
+          ? -this.shapePx.x
+          : -this.shape.size.x.size
+      const MIN_Y =
+        this.shape.size.y.unit === '%'
+          ? -this.shapePx.y
+          : -this.shape.size.y.size
+      return { min: { x: MIN_X, y: MIN_Y } }
+    }
   }
 }
 </script>
