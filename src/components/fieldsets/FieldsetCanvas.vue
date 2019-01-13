246e755dd4ccf7b837885d14c6df93e05e7bfad0
vicbergquist <victoria.bergquist@gmail.com>
2019-01-13 18:21:39 +0100

    max canvas values

--- src/components/fieldsets/FieldsetCanvas.vue
+++ src/components/fieldsets/FieldsetCanvas.vue
@@ -12,7 +12,7 @@
         :label="'Width'"
         :name="'canvas-width'"
         :what="canvas.x"
-        :max="maxX"
+        :max="canvas.x.unit === 'px' ? canvas.x.max : 100"
       />
 
       <!-- HEIGHT INPUT -->
@@ -21,7 +21,7 @@
         :label="'Height'"
         :name="'canvas-height'"
         :what="canvas.y"
-        :max="maxY"
+        :max="canvas.y.unit === 'px' ? canvas.y.max : 100"
       />
     </template>
   </div>
@@ -30,7 +30,6 @@
 <script>
 import InputWrapper from '@/components/InputWrapper'
 import GroupToggle from '@/components/GroupToggle'
-import { mapGetters } from 'vuex'
 import { createHelpers } from 'vuex-map-fields'
 const { mapFields } = createHelpers({
   getterType: 'canvas/getField',
@@ -48,38 +47,7 @@ export default {
     desc: "Edit the size of your canvas. Choose 'px' by clicking on the unit."
   }),
   computed: {
-    ...mapFields(['canvas']),
-    ...mapGetters(['canvas/canvas']),
-    maxX() {
-      if (this.canvas.x.unit === 'px') return this.canvas.x.max
-      else return 100
-    },
-    maxY() {
-      if (this.canvas.y.unit === 'px') return this.canvas.y.max
-      else return 100
-    }
+    ...mapFields(['canvas'])
   }
 }
 </script>
-
-<style lang="sass" scoped>
-
-.canvas-size
-  max-width: 50%
-  display: flex
-  flex-direction: column-reverse
-
-label
-  text-align: center
-  margin-top: .2rem
-
-input
-  border-radius: 2rem
-  display: block
-  font-size: 1.2rem
-  max-width: 150px
-  text-align: center
-  border: 1px solid $black
-  &:focus
-    border: 1px solid $green
-</style>
