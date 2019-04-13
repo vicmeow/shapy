3d48d18559da37d791d1ddfd1a0c149756895dde
vicbergquist <victoria.bergquist@gmail.com>
2019-04-13 17:31:09 +0200

    add code snippet component

---
+++ components/editor/CodeSnippet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="code-snippet"></div>
+</template>
+
+<script>
+/* 
+CODE SNIPPET
+  FEATURES:
+    - Syntax highlighting
+    - [BUTTON] Toggle shorthand
+    - [BUTTON] Show all colors
+    - [BUTTON] Show list of gradients (layers)
+    - [BUTTON] Submit?
+    - [BUTTON] Copy CSS
+    - [BUTTON] Export (CodePen)
+*/
+</script>
