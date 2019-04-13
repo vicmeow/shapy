a36713d2a812c15b760b29d6bb001c765277bc04
vicbergquist <victoria.bergquist@gmail.com>
2019-04-13 17:28:26 +0200

    add shape store file

---
+++ store/shape.js
@@ -0,0 +1,90 @@
+import { getField, updateField } from 'vuex-map-fields'
+
+export const state = () => ({
+  width: {
+    pct: 17,
+    px: null,
+    max: null,
+    defaultUnit: true
+  },
+  height: {
+    pct: 17,
+    px: null,
+    max: null,
+    defaultUnit: true
+  },
+  x: {
+    pct: 50,
+    px: null,
+    max: null,
+    defaultUnit: true
+  },
+  y: {
+    pct: 50,
+    px: null,
+    max: null,
+    defaultUnit: true
+  }
+})
+
+export const getters = {
+  getField,
+  shape(state) {
+    // Which width unit to return based on defaultUnit
+    const width = state.width.defaultUnit
+      ? `${state.width.pct}%`
+      : `${state.width.px}px`
+    // Which height unit to return based on defaultUnit
+    const height = state.height.defaultUnit
+      ? `${state.height.pct}%`
+      : `${state.height.px}px`
+    // Which X coordinate unit to return based on defaultUnit
+    const x = state.x.defaultUnit ? `${state.x.pct}%` : `${state.x.px}px`
+    // Which Y coordinate unit to return based on defaultUnit
+    const y = state.y.defaultUnit ? `${state.y.pct}%` : `${state.y.px}px`
+    // Finished string for the gradient's coordinates and size
+    return `${width} ${height} at ${x} ${y}`
+  }
+}
+
+export const mutations = {
+  updateField,
+  updateShape(state, shape) {
+    state.shape = shape
+  },
+  updateWidth(state, max) {
+    // If px values for box size are null, set them to the max
+    if (!state.width.px) state.width.px = max
+
+    // If px values for the axis are null, set them to the max
+    if (!state.x.px) state.x.px = max
+
+    // Box max values should be the the canvas px max
+    state.width.max = max
+
+    // If px values exist for the axis, set them as the payload
+    state.x.max = max
+  },
+  updateHeight(state, max) {
+    // If px values for box size are null, set them to the max
+    if (!state.height.px) state.height.px = max
+
+    // If px values for the axis are null, set them to the max
+    if (!state.y.px) state.y.px = max
+
+    // Box max values should be the the canvas px max
+    state.height.max = max
+
+    // If px values exist for the axis, set them as the payload
+    state.y.max = max
+  }
+}
+export const actions = {
+  updateMax({ commit }, max) {
+    // Update max width if the width is changing
+    if (max.type === 'width') commit('updateWidth', max.value)
+    // Update max height if the height is changing
+    if (max.type === 'height') commit('updateHeight', max.value)
+  }
+}
+// TODO: Fix max values for the radial shape, calculated based on box size
