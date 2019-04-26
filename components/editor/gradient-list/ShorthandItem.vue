8eb0c23806315aa6d3471a68516825c0fe21fcb8
vicbergquist <victoria.bergquist@gmail.com>
2019-04-26 11:33:31 +0200

    add item in gradient list

---
+++ components/editor/gradient-list/ShorthandItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="gradient-item">
+    <button @click="editGradient(item.id)">
+      edit
+    </button>
+    <div
+      :style="{
+        'background-image': item.image,
+        height: '1.5rem',
+        width: '1.5rem'
+      }"
+    ></div>
+    <span>{{ item.background }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  methods: {
+    editGradient(id) {
+      this.$store.dispatch('gradients/editGradient', id)
+    }
+  }
+}
+</script>
