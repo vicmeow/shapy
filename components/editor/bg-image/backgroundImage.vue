39cd7766f549d867073d4931ec8d556e1b4792a5
vicbergquist <victoria.bergquist@gmail.com>
2019-04-26 19:42:08 +0200

    add angles

--- components/editor/bg-image/backgroundImage.vue
+++ components/editor/bg-image/backgroundImage.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="bg-image">
-    <h3 class="heading">Background-image</h3>
-    <div class="general-settings">
-      <input-wrapper class="gradient-repeating">
+    <h2 class="heading">Background-image</h2>
+    <h3>Type</h3>
+    <div class="gradient-type">
+      <input-wrapper>
+        <radio-input
+          v-for="radio in types"
+          :key="radio.index"
+          v-model="activeType"
+          :active="activeType"
+          :radio="radio"
+          group="gradient-type"
+        />
+      </input-wrapper>
+      <input-wrapper>
         <label>Repeating</label>
         <toggle-input
           v-model="repeating"
@@ -10,17 +21,31 @@
           name="repeating-gradient"
         ></toggle-input>
       </input-wrapper>
-      <input-wrapper class="gradient-type">
+    </div>
+    <div v-if="activeType === 'linear'" class="gradient-angle">
+      <div class="angle-control">
+        <div
+          class="angle-point-wrapper"
+          :style="{
+            transform: `rotate(${activeAngle}deg)`
+          }"
+        >
+          <div class="angle-point"></div>
+        </div>
+      </div>
+      <span>{{ activeAngle }}deg</span>
+      <input-wrapper>
         <radio-input
-          v-for="radio in type.types"
+          v-for="radio in angles"
           :key="radio.index"
-          v-model="type.active"
-          :active="type.active"
+          v-model.number="activeAngle"
+          :active="activeAngle"
           :radio="radio"
-          group="gradient-type"
+          group="gradient-angle"
         />
       </input-wrapper>
     </div>
+    <h3>Colors</h3>
     <color-stops />
   </div>
 </template>
@@ -41,8 +66,16 @@ export default {
   computed: {
     ...mapFields({
       repeating: 'backgroundImage.repeating',
-      type: 'backgroundImage.type'
-    })
+      activeType: 'backgroundImage.type.active',
+      activeAngle: 'backgroundImage.angle.active',
+      comment: 'backgroundImage.comment'
+    }),
+    types() {
+      return this.$store.state.backgroundImage.type.types
+    },
+    angles() {
+      return this.$store.state.backgroundImage.angle.types
+    }
   }
 }
 </script>
@@ -51,7 +84,30 @@ export default {
 .bg-image
   // outline: 1px solid red
 
-.general-settings
+.gradient-type
   display: flex
-  // justify-content: space-between
+  flex-wrap: wrap
+
+.angle-control
+  height: 1.5rem
+  width: 1.5rem
+  border-radius: 50%
+  border: 2px solid $black
+  display: flex
+  align-items: center
+  justify-content: center
+
+.angle-point-wrapper
+  position: relative
+  height: 100%
+  width: 5px
+  pointer-events: none
+
+.angle-point
+  position: absolute
+  top: 2.5px
+  height: 5px
+  width: 5px
+  border-radius: 50%
+  background: black
 </style>
