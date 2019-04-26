3f0eaa618c97b9acbd60fa5e050bf215594724c0
vicbergquist <victoria.bergquist@gmail.com>
2019-04-26 11:30:37 +0200

    add gradient list component

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="page-editor">
-    <editor-controls />
-    <no-ssr>
-      <Canvas />
-    </no-ssr>
+    <component :is="component" @showList="showList = !showList" />
+    <div class="fixed">
+      <no-ssr>
+        <Canvas />
+      </no-ssr>
+    </div>
   </div>
 </template>
 
 <script>
 import Canvas from '@/components/editor/Canvas'
 import EditorControls from '@/components/editor/EditorControls'
+import GradientList from '@/components/editor/gradient-list/GradientList'
+import { mapState, mapGetters } from 'vuex'
 export default {
   components: {
     Canvas,
-    EditorControls
+    EditorControls,
+    GradientList
+  },
+  data() {
+    return {
+      shorthand: false,
+      showList: false
+    }
+  },
+  computed: {
+    component() {
+      return this.showList ? 'gradient-list' : 'editor-controls'
+    },
+    ...mapState({
+      bgSize: 'backgroundSize'
+    }),
+    ...mapGetters({
+      background: 'background',
+      bgImage: 'backgroundImage',
+      bgSize: 'backgroundSize',
+      bgPosition: 'backgroundPosition',
+      bgRepeat: 'backgroundRepeat'
+    })
   }
 }
 </script>
 
 <style lang="sass">
+
 .page-editor
+  position: relative
+  display: flex
+
+.fixed
+  position: sticky
+  top: 2.5rem
+  right: 0
+  height: calc(100vh - 4.2rem)
+  display: flex
+  flex-direction: column
+  flex-grow: 1
   padding: 0 1rem
 </style>
