826f24f43d324b7aae6786b391fe24eefe861e41
vicbergquist <victoria.bergquist@gmail.com>
2019-04-30 19:44:40 +0200

    add control info component

---
+++ components/editor/controls/ControlInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="control-info">
+    <div class="info-icon">?</div>
+    <div class="info-description">
+      About control
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+.control-info
+  position: absolute
+  top: 0
+  right: 0
+
+.info-icon
+  color: $darkgrey
+  padding: 1rem
+
+.control-info:hover .info-description
+  display: block
+
+.info-description
+  display: none
+  position: absolute
+  left: -300px
+  left: 0
+  top: -2rem
+  background: $white
+  // box-shadow: $boxShadow
+  border-radius: $borderRadius
+  height: 100px
+  width: 300px
+  padding: 1rem
+  z-index: 800
+</style>
