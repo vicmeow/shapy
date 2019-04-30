cd47710981a470bec45fb10ea93dc830459fae4f
vicbergquist <victoria.bergquist@gmail.com>
2019-04-30 19:46:11 +0200

    add bg repeat descriptions

--- components/editor/controls/backgroundRepeat.vue
+++ components/editor/controls/backgroundRepeat.vue
@@ -1,24 +1,43 @@
 <template>
   <div class="control repeat-control">
-    <h3 class="heading">Repeat</h3>
+    <h2 class="heading">Repeat</h2>
     <radio-input
-      v-for="value in backgroundRepeat.values"
-      :key="value.index"
+      v-for="(radio, index) in backgroundRepeat.values"
+      :key="index"
       v-model="backgroundRepeat.active"
       :active="backgroundRepeat.active"
-      :radio-value="value"
-      :label="value"
+      :radio-value="radio.value"
+      :label="radio.value"
       group="background-repeat"
+      class="option"
+    >
+      <component
+        :is="`icon-${radio.value}`"
+        :active="backgroundRepeat.active === radio.value"
     />
+      <!-- <p slot="description" class="description">{{ radio.description }}</p> -->
+    </radio-input>
   </div>
 </template>
 
 <script>
+import IconRepeat from '@/components/icons/IconRepeat'
+import IconNoRepeat from '@/components/icons/IconNoRepeat'
+import IconRepeatX from '@/components/icons/IconRepeatX'
+import IconRepeatY from '@/components/icons/IconRepeatY'
+import IconSpace from '@/components/icons/IconSpace'
+import IconRound from '@/components/icons/IconRound'
 import RadioInput from '@/components/inputs/RadioInput'
 import { mapFields } from 'vuex-map-fields'
 export default {
   components: {
-    RadioInput
+    RadioInput,
+    IconNoRepeat,
+    IconRepeat,
+    IconRepeatX,
+    IconRepeatY,
+    IconSpace,
+    IconRound,
   },
   computed: {
     ...mapFields({
@@ -28,7 +47,13 @@ export default {
 }
 </script>
 
-<style lang="sass">
-.repeat-control
+<style lang="sass" scoped>
+.repeat-options
   display: flex
+  flex-wrap: wrap
+  justify-content: space-between
+  min-width: 0
+
+.option
+  flex-basis: 25%
 </style>
--- store/backgroundRepeat.js
+++ store/backgroundRepeat.js
@@ -2,7 +2,35 @@ import { getField, updateField } from 'vuex-map-fields'
 
 export const state = () => ({
   active: 'no-repeat',
-  values: ['no-repeat', 'repeat', 'space', 'round', 'repeat-x', 'repeat-y']
+  values: [
+    {
+      value: 'no-repeat',
+      description: "Don't repeat the gradient."
+    },
+    {
+      value: 'repeat',
+      description:
+        'Repeat the gradient to cover the canvas and clip it if necessary.'
+    },
+    {
+      value: 'repeat-x',
+      description: 'Repeat the gradient along the x axis.'
+    },
+    {
+      value: 'repeat-y',
+      description: 'Repeat the gradient along the y axis.'
+    },
+    {
+      value: 'space',
+      description:
+        'Repeat the gradient as many times as possible without clipping it. Ignores background-position unless the gradient can be displayed without clipping.'
+    },
+    {
+      value: 'round',
+      description:
+        'Stretch the gradient to fit as many repetitions as possible without clipping and leaving no caps.'
+    }
+  ]
 })
 
 export const getters = {
